965f3fd7868797316a47f708a5980165e6b2c4d5
권영호 <dudgh0958@pood.pet>
2023-02-27 17:55:04 +0900

    repository 일단 삭제

--- src/main/java/com/example/payment/service/PaymentService.java
+++ src/main/java/com/example/payment/service/PaymentService.java
@@ -2,17 +2,10 @@ package com.example.payment.service;
 
 import com.example.payment.entity.Order;
 import com.example.payment.repository.PaymentRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class PaymentService {
-
-    private final PaymentRepository paymentRepository;
-
     public void insert(final Order order) {
-        paymentRepository.save(order);
-
     }
 }
